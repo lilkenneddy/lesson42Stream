91f454968cdeeb50fb5b0747f290048d2a2997b1
Rostyslav Skliaruk <sklyaruk.rostislav1903@gmail.com>
2025-04-29 19:11:11 +0200

    completed homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
+import org.example.lesson42Stream.Student;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Student> studentList = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student student1 = new Student("Mark", 3.9, 18);
+        Student student2 = new Student("Danya", 4.1, 21);
+        Student student3 = new Student("Vova", 4.0, 20);
+        Student student4 = new Student("Rostik", 5.0, 85);
+
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+        studentList.stream()
+                .filter(n -> n.getGrade() < 4.0)
+                .forEach(System.out::println);
+
+        Optional<Student> bestStudent = studentList.stream()
+                .max(Comparator.comparingDouble(Student::getGrade));
+        bestStudent.ifPresent(System.out::println);
+
+        long count = studentList.stream()
+                .filter(n -> n.getAge() > 20)
+                .count();
+        System.out.println(count);
+
+        List<Student> sortedListOfStudents = studentList.stream()
+                .sorted(Comparator.comparing(Student::getGrade).reversed())
+                .collect(Collectors.toList());
+        System.out.println(sortedListOfStudents);
+
+        boolean hasMaxGrade = studentList.stream()
+                .anyMatch(n -> n.getGrade() == 5.0);
+        System.out.println(hasMaxGrade);
+
+        studentList.stream()
+                .filter(n -> n.getGrade() > 4.0)
+                .forEach(n -> System.out.println(n.getName()));
+
     }
 }
